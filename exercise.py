df925106ef54e70e97cbbf2753818290bca579cd
Natalie Gokkaya <natkaya@yahoo.com>
2025-09-24 15:21:49 +0100

    Finding the best and worse days over 2 data sets

---
+++ exercise.py
@@ -0,0 +1,17 @@
+# sales_w1 = [7,3,42,19,15,35,9]
+# sales_w2 = [12,4,26,10,7,28]
+# sales = []
+
+
+# sales = sales_w1 + sales_w2
+# # alternative method would have been
+# # sales.extend(sales_w1) ~have to add one by one
+# # sales.extend(sales_w2)
+# print(sales)
+# print(min(sales)      )
+# print(max(sales)      )
+# # alternative would have been to sort and extract first and last element
+# sales.sort()
+# print(sales[0])
+# print(sales[-1])
+
