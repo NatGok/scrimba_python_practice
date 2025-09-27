c2c1745ec43ce9bedddf373fec6560faafd7cfc0
Natalie Gokkaya <natkaya@yahoo.com>
2025-09-27 18:03:15 +0100

    Click start challenges

--- Spathiphyllum.py
+++ Spathiphyllum.py
@@ -9,3 +9,26 @@ elif customer_choice == "Spathiphyllum":
     print("Yes - Spathiphyllum is the best plant ever!")
 else: 
     print("Spathiphyllum! Not " + customer_choice + "!")
+#exercise 2
+income = float(input("Enter the annual income: "))
+
+if income < 85528:
+	tax = income * 0.18 - 556.02
+# Write the rest of your code here.
+else:
+    tax =((14839.02-income)*.032)+14839.02
+
+if tax < 0.0:
+	tax = 0.0
+     
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
+ #exercise3 leap year or common year or pre georgian calendar
+year = int(input("Enter a year: "))
+if year < 1582:
+    print("This year is before the Gregorian calendar.")
+elif year % 4 == 0 and (year % 100 != 0):
+    print("This is a leap year.")
+else year % 400 != 0:
+		print("Common year")
+    print("This is a common year.")
